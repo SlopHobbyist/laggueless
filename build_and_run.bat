1eff59b7787c028308063f6216e052d2f8d4a831
Stewie Griffin <example@example.com>
2026-06-20 07:55:26 -0400

    build script update rom name

--- build_and_run.bat
+++ build_and_run.bat
@@ -83,7 +83,7 @@ if defined FAILED (
     echo.
     echo [WARN] Some steps failed:%FAILED% -- attempting launch anyway
 )
-"%ROOT%release\laggueless.exe" ".\release\cores\mesen_libretro.dll" ".\release\roms\Super Mario Bros. (World).nes" --vulkan --lsfg
+"%ROOT%release\laggueless.exe" "%ROOT%release\cores\mesen_libretro.dll" "%ROOT%release\roms\Super Mario Bros. (Japan, USA).nes" --vulkan --lsfg
 if errorlevel 1 (
     echo [FAIL] laggueless.exe exited with error
     set FAILED=%FAILED% launch
